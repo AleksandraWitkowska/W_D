b138d60d4e6b4849485e4a358883a3b9cbbe43e1
AleksandraWitkowska <93096976+AleksandraWitkowska@users.noreply.github.com>
2022-03-11 19:39:09 +0100

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,96 @@
+import math
+
+zad1
+a = 2
+b = 77
+c = 2.2
+d = 8.8
+lz1 = 2+5j
+lz2 = 3+4j
+lancuch1 = 'informatyka niestacjonarne'
+lancuch2 = 'byle do czerwca'
+print(a,b, c,d)
+print(lz1,  lz2, lancuch1)
+print(lancuch2)
+
+zad2
+print("Wybierz dzialanie")
+print("a. dodawanie")
+print("b. odejmowanie")
+print("c. mnożenie")
+print("d. dzielenie")
+
+wybor = input("Co wybierasz? (a/ b/ c/ d): ")
+
+num_1 = int(input("Podaj pierwszą liczbę "))
+num_2 = int(input("Podaj drugą liczbę: "))
+
+if wybor == 'a':
+    print(num_1, " + ", num_2, " = ",(num_1+num_2))
+
+elif wybor == 'b':
+    print(num_1, " - ", num_2, " = ",(num_1-num_2))
+
+elif wybor == 'c':
+    print(num1, " * ", num2, " = ",(num1*num2))
+elif wybor == 'd':
+    print(num_1, " / ", num_2, " = ", (num_1/num_2))
+else:
+    print("Błąd")
+
+zad3
+a = 88
+a += 22
+print (a)
+b = 6.8
+b -= 7.9
+print(format(b,".2f"))
+c = 2+7j
+c *= 4
+print(c)
+d = 77.9
+d /= 11.3
+print (d)
+e = 599999
+e **= 4
+print(e)
+f = 6666
+f %= 5
+print (f)
+
+zad4
+x = pow(math.e,10)
+print (x)
+b = math.ceil(4.80)
+c = math.floor(3.55)
+print(b,c)
+d = (math.log(5+(math.sin(8)**2))**(1/6))
+print (d)
+
+zad5
+imie = 'ALEKSANDRA'
+nazwisko = 'WITKOWSKA'
+x = imie.capitalize()
+y = nazwisko.capitalize()
+print (x,y)
+
+zad6
+piosenka = 'Uh la la la, Uh la la la la, Uh la la la, Uh la la la la, Uh la la la, I love you baby'
+ile_la = piosenka.count("la")
+print (ile_la)
+
+zad7
+text = 'jamnik szorstkowłosy'
+print (text[1], text [19])
+
+zad8
+piosenka = 'Uh la la la, Uh la la la la, Uh la la la, Uh la la la la, Uh la la la, I love you baby'
+x = piosenka.split()
+print (x)
+
+zad9
+string = 'Działanie antywirusowe jest pośrednim efektem aktywacji enzymów cytoplazmatycznych'
+float = 5.55
+szesnastkowy = hex(567)
+print ('%s, %f, %s' %(string, float, szesnastkowy))
+
